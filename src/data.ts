1b056fa8f8d4b577e15f7c438b63e62870d9f9d7
Marveeen <jmvroqueee@gmail.com>
2024-12-14 22:06:25 +0800

    feat: created running-routine component

---
+++ src/data.ts
@@ -0,0 +1,19 @@
+export default [
+  { day: "Mon", run: "Easy Run", distance: "5 km", duration: "30 mins" },
+  { day: "Tue", run: "Rest Day", distance: "-", duration: "-" },
+  {
+    day: "Wed",
+    run: "Interval Training",
+    distance: "6 km",
+    duration: "45 mins",
+  },
+  { day: "Thu", run: "Easy Run", distance: "5 km", duration: "30 mins" },
+  { day: "Fri", run: "Rest Day", distance: "-", duration: "-" },
+  { day: "Sat", run: "Long Run", distance: "10 km", duration: "60 mins" },
+  {
+    day: "Sun",
+    run: "Recovery Run",
+    distance: "3 km",
+    duration: "20 mins",
+  },
+];
